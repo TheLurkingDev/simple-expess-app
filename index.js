4054eaf5bbbe8a5798c27957f5ba230f1fd7edef
TheLurkingDev <thelurkingdev@gmail.com>
2018-08-22 22:14:05 -0400

    Finished with three get requests: one returning text/html, one returning application/json, and one taking a parameter. Also created simple matching requests in Postman.

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send('Hello world!');    
+});
+
+app.get('/json', (req, res) => {
+    res.json({ userName: 'Darren' });
+});
+
+app.get('/variable/:value', (req, res) => {
+    res.send('You entered: ' + req.params.value);
 });
 
 app.listen(3000, () => {
